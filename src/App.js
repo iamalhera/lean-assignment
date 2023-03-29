5465cf0c2ee8f27ce26552e5a20e6c3d08ea23f7
Al Hera Ahmad <alheraahmad0786@gmail.com>
2023-03-29 22:46:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 import Homepage from './Pages/Homepage/Homepage';
 import Signin from './components/signin/Signin';
 import Signup from './components/signup/Signup';
-// import { Link } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App_wrapper">
       <Navbar />
-      {/* <div>
-        <Link to="/" >Homepage</Link> ||
-        <Link to="/signin" >Signin</Link> ||
-        <Link to="/signup" >Signup</Link>
-      </div> */}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="signin" element={<Signin />} />
